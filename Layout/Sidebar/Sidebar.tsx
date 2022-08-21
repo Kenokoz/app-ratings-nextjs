3574ce827efb6aa279b1bc1d01e48ba43dfc45aa
Kirill Andreev <kenokoz@yandex.ru>
2022-08-21 20:22:31 +0300

    add logo for sidebar

--- Layout/Sidebar/Sidebar.tsx
+++ Layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
+import clsx from 'clsx';
 import { Menu } from '../Menu/Menu';
 import styles from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
+import Logo from '../logo.svg';
 
-export const Sidebar: FC<SidebarProps> = ({ ...props }) => {
+export const Sidebar: FC<SidebarProps> = ({ className, ...props }) => {
 	return (
-		<div {...props}>
+		<div className={clsx(styles.sidebar, className)} {...props}>
+			<Logo className={styles.logo} />
+			<div>Поиск</div>
 			<Menu />
 		</div>
 	);
